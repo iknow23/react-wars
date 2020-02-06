98e867dcd56c61301e705b7dd3d4ba2a5e9a234b
Aynur Vafin <aynur.vafin@simbirsoft.com>
2020-02-06 17:18:59 +0300

    add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-eclipse-mmimdka6mnl">
+      <div class="ldio-a0mw557r9x6">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
